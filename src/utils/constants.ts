70a47c936db22e7379cacb3a1f639feef63a5894
havelessbemore <dev.michael.rojas@gmail.com>
2024-02-14 09:16:51 -0500

    Add ARGS_MAX_LENGTH constant

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,16 @@
 /**
+ * The maximum number of function arguments.
+ *
+ * In theory, arguments can be of any length.
+ * In practice, implementations impose different limits.
+ *
+ * @see {@link https://262.ecma-international.org/14.0/#sec-list-and-record-specification-type | ECMAScript 2023}
+ */
+export const ARGS_MAX_LENGTH = 16383; // 2**14 - 1
+
+/**
  * The maximum length of an array. The value is an unsigned 32-bit integer.
  *
- * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/length}
+ * @see {@link https://262.ecma-international.org/14.0/#sec-arraycreate | ECMAScript 2023}
  */
 export const ARRAY_MAX_LENGTH = 4294967295; // 2**32 - 1;
