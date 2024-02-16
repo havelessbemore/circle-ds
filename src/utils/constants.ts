f0ada2fc1dbe19e02dea70ef82fe98c76707a3e1
havelessbemore <dev.michael.rojas@gmail.com>
2024-02-15 23:02:21 -0500

    Update documentation for utils/constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,16 +1,26 @@
 /**
- * The maximum number of function arguments.
+ * Represents the maximum number of arguments that can be passed to a function.
  *
- * In theory, arguments can be of any length.
- * In practice, implementations impose different limits.
+ * While ECMAScript specifications do not explicitly limit the number of function arguments,
+ * practical limits are imposed by JavaScript engine implementations. Different engines can
+ * have different maximums, so a conservative value is used to prevent runtime errors.
  *
- * @see {@link https://262.ecma-international.org/14.0/#sec-list-and-record-specification-type | ECMAScript 2023}
+ * For more details, see the
+ * {@link https://262.ecma-international.org/14.0/#sec-list-and-record-specification-type | ECMAScript Specification},
+ * section "6.2.2 The List and Record Specification Types".
  */
 export const ARGS_MAX_LENGTH = 16383; // 2**14 - 1
 
 /**
- * The maximum length of an array. The value is an unsigned 32-bit integer.
+ * Defines the maximum allowable length of an array.
  *
- * @see {@link https://262.ecma-international.org/14.0/#sec-arraycreate | ECMAScript 2023}
+ * According to the ECMAScript specification, the length property of an array is
+ * an unsigned 32-bit integer, so its range is 0 to 2**32 - 1, inclusive. Beyond this,
+ * operations that modify the array length (e.g., `push`, `pop`, `unshift`, `shift`)
+ * may fail or behave unexpectedly.
+ *
+ * For more details, see the
+ * {@link https://262.ecma-international.org/14.0/#sec-arraycreate | ECMAScript Specification},
+ * section "10.4.2.2 ArrayCreate".
  */
 export const ARRAY_MAX_LENGTH = 4294967295; // 2**32 - 1;
