e2a728415f631b9b27c031c21a0f1b468d05d5be
havelessbemore <dev.michael.rojas@gmail.com>
2024-03-14 19:28:21 -0400

    Update tests for linked node data strctures to use the LINKED_MAX_LENGTH constant

--- src/circle/circularDoublyLinkedList.test.ts
+++ src/circle/circularDoublyLinkedList.test.ts
@@ -1,8 +1,9 @@
 import { test as testBoundedCollection } from "../../tests/boundedCollection";
 import { testBoundedList } from "../../tests/boundedList";
 import { testList } from "../../tests/list";
+import { LINKED_MAX_LENGTH } from "../utils/constants";
 import { CircularDoublyLinkedList } from "./circularDoublyLinkedList";
 
 testList(CircularDoublyLinkedList);
-testBoundedCollection(CircularDoublyLinkedList, Number.MAX_SAFE_INTEGER);
+testBoundedCollection(CircularDoublyLinkedList, LINKED_MAX_LENGTH);
 testBoundedList(CircularDoublyLinkedList);
--- src/circle/circularLinkedDeque.test.ts
+++ src/circle/circularLinkedDeque.test.ts
@@ -1,7 +1,8 @@
 import { test as testBoundedCollection } from "../../tests/boundedCollection";
 import { test as testBoundedDeque } from "../../tests/boundedDeque";
+import { LINKED_MAX_LENGTH } from "../utils/constants";
 
 import { CircularLinkedDeque } from "./circularLinkedDeque";
 
-testBoundedCollection(CircularLinkedDeque, Number.MAX_SAFE_INTEGER);
+testBoundedCollection(CircularLinkedDeque, LINKED_MAX_LENGTH);
 testBoundedDeque(CircularLinkedDeque);
